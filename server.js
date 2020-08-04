27e037fb15d21c473d58e63333f1ce6897247e34
Stephen Myers <myersmstephen@gmail.com>
2020-08-04 17:51:57 -0400

    chore: add express.json builtin middleware

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ dotEnv.config(); //load the .env into process.env so we can use it later
 const app = express();
 const PORT = process.env.PORT || 3000; //try to fetch the port from process environment or default the port to 3000
 
+/*
+BuiltIn Middleware for handling json and url-encoded payload 
+*/
+app.use(express.json()); //Request Content-Type "application/json"
+
 app.get("/", (request, response, next) => {
   response.send("Node API Server");
 });
