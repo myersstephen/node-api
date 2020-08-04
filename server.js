b9468b45d27ad9e405a13971a7971261718ec84a
Stephen Myers <myersmstephen@gmail.com>
2020-08-04 18:11:21 -0400

    chore: add third party middleware and npm package cors

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const dotEnv = require("dotenv");
+const cors = require("cors");
+
 dotEnv.config(); //load the .env into process.env so we can use it later
 const app = express();
 const PORT = process.env.PORT || 3000; //try to fetch the port from process environment or default the port to 3000
 
-/*
-BuiltIn Middleware for handling json and url-encoded payload 
-*/
+/**
+ * BuiltIn Middleware for handling json and url-encoded payload
+ */
 app.use(express.json()); //Request Content-Type "application/json"
 app.use(express.urlencoded({ extended: true })); //Request Content-Type: "application/x-www-form-urlencoded"
 
+/**
+ * Third Party Middleware CORS - For any cross origin request
+ * Cross-Origin Resource Sharing Enablement on server
+ */
+app.use(cors());
 app.get("/", (request, response, next) => {
   response.send("Node API Server");
 });
