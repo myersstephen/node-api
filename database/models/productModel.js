79e178ffbc7212326aa880c3488bacdeacca9641
Stephen Myers <myersmstephen@gmail.com>
2020-08-05 14:48:36 -0400

    refactor: add toObject and transform to product model to be able to remove sensitive info

--- database/models/productModel.js
+++ database/models/productModel.js
@@ -10,6 +10,17 @@ const productSchema = new mongoose.Schema(
   },
   {
     timestamps: true, //mongoose to assign createdAt and updatedAt timestamp
+    //https://mongoosejs.com/docs/api.html#document_Document-toObject
+    toObject: {
+      transform: function (doc, ret, options) {
+        //when transform is called in toObject we can manipulate
+        //the document and remove sensitive information
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
   }
 );
 
--- service/productService.js
+++ service/productService.js
@@ -8,12 +8,12 @@ const Product = require("../database/models/productModel");
  */
 module.exports.createProduct = async (productServiceData) => {
   try {
-    //console.log(serviceData);
-    //Insert Data into db, pass data into new Product Model instance
-    //Use spread operater because key and value are the same ...serviceData
-    //Or name: serviceData.name, etc
     let product = new Product({ ...productServiceData });
-    return await product.save(); //save is a promise so we must await
+    let result = await product.save();
+    //to Object method converts document to a Object - applied on single instance
+    //doesnt affect data in db
+    //if multiple documents we need to iterate over them all an apply toObject
+    return result.toObject();
   } catch (error) {
     console.log("Mongoose Save Document Error: Service: createProduct ", error);
   }
